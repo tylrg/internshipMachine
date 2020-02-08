d82af8cd6c669ca8e1ca09b7f4b760c6ccc3d398
Tyler Gabriel <tygabriel7@gmail.com>
2020-02-08 14:37:15 -0500

    Formatting
    Got rid of test files and added components
    
    Co-Authored-By: Jesse White <jwhite48@users.noreply.github.com>
    Co-Authored-By: bwilso14 <bwilso14@users.noreply.github.com>
    Co-Authored-By: benjpicone <benjpicone@users.noreply.github.com>

--- front/front-end/src/app/app.module.ts
+++ front/front-end/src/app/app.module.ts
@@ -4,15 +4,28 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { HeaderComponent } from './header/header.component';
+import { DataComponent } from './data/data.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    DataComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatDividerModule,
+    MatToolbarModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
