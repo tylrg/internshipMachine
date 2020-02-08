c68c8c251c5b1bae5794c83a74e597fda4853ee6
Tyler Gabriel <tygabriel7@gmail.com>
2020-02-08 17:16:32 -0500

    Added data components
    Co-Authored-By: Jesse White <jwhite48@users.noreply.github.com>
    Co-Authored-By: bwilso14 <bwilso14@users.noreply.github.com>
    Co-Authored-By: benjpicone <benjpicone@users.noreply.github.com>

--- front/front-end/src/app/app.component.ts
+++ front/front-end/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 
 
+
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,4 +10,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'front-end';
+  
+  portfolio = [
+    { "name": "Nike", "symbol": "NKE", "price": "5.23", "score": "7.8", "extra":"-43.%"},
+    { "name": "Microsoft", "symbol": "MSFT", "price": "5.23", "score": "7.8", "extra":"-43.%"},
+    { "name": "Google", "symbol": "ABC", "price": "444.333", "score": "7.8", "extra":"-43.%"},
+  ]; 
 }
--- front/front-end/src/app/data/data.component.ts
+++ front/front-end/src/app/data/data.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-data',
@@ -8,6 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class DataComponent implements OnInit {
 
   constructor() { }
+  @Input()
+  name: string = "";
+  @Input()
+  symbol: string = "";
+  @Input()
+  score: string = "";
+  @Input()
+  price: string = "";
+  @Input()
+  extra: string = "";
 
   ngOnInit() {
   }
