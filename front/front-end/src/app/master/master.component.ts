a7c702e82a49b326e320bac5f3a2daa614aed350
Tyler Gabriel <tygabriel7@gmail.com>
2020-02-09 10:02:14 -0500

    Pushing front end

--- front/front-end/src/app/master/master.component.ts
+++ front/front-end/src/app/master/master.component.ts
@@ -11,7 +11,7 @@ export class MasterComponent implements OnInit {
   constructor(public data:DataService) { }
 
   ngOnInit() {
-    console.log("VERSION 0.0.8");
+    console.log("VERSION 0.0.9");
   }
 
   portfolio = [
@@ -24,11 +24,10 @@ export class MasterComponent implements OnInit {
   refresh() {
     console.log("Refreshing");
     this.updatePrices();
+    this.
     this.calculateMetric();
 
-    // this.data.getSentiment().subscribe(res => {
-    //   console.log(res);
-    // });
+
   }
 
   //opens a help prompt
@@ -45,14 +44,12 @@ export class MasterComponent implements OnInit {
   clear() {
     console.clear();
   }
-
   add() {
     let addSymbol = (<HTMLInputElement>document.getElementById("addField")).value;
     let addName = (<HTMLInputElement>document.getElementById("addName")).value;
-    console.log("adding "+addSymbol+" ");
+    //console.log("adding "+addSymbol+" ");
     this.portfolio.push({ "name": addName, "symbol": addSymbol, "price": "0.00", "score": "0.0", "extra": "0.0%","sentimentAvg":"0.0","magnitude":"0.0" });
   }
-
   calculateMetric(){
 
     for(let stock of this.portfolio){
@@ -73,30 +70,51 @@ export class MasterComponent implements OnInit {
       }
       let value = (numerator/denominator);
       stock.score=value.toString();
-      console.log(JSON.stringify(stock.score));
+      //console.log(JSON.stringify(stock.score));
     }
   }
-
   updatePrices(){
     for (let stock of this.portfolio) {
       
       let sym = stock.symbol;
       //console.log(JSON.stringify(stock.symbol));
       this.data.getPercent(sym).subscribe(res => {
-        console.log(res);
+        //console.log(res);
         let jString = JSON.stringify(res);
         let result = JSON.parse(jString);
         stock.extra=result.value;
-        console.log(JSON.stringify(stock.extra));
+        //console.log(JSON.stringify(stock.extra));
       });
 
       this.data.getPrice(sym).subscribe(res => {
-        console.log(res);
+        //console.log(res);
         let jString = JSON.stringify(res);
         let result = JSON.parse(jString);
         stock.price = result.value;
-        console.log(JSON.stringify(stock.price));
+        //console.log(JSON.stringify(stock.price));
       });      
+    }
+  }
+  updateSentiment(){
+    for (let stock of this.portfolio) {
+
+      let sym = stock.symbol;
+      let name=stock.name;
+      this.data.getSentiment(sym,name).subscribe(res => {
+        console.log(res);
+        let jString = JSON.stringify(res);
+        let result = JSON.parse(jString);
+        let sentimentString=result.value;
+        let space = sentimentString.indexOf(' ');
+        console.log(space);
+        let magnitude = sentimentString.substring(space);
+        sentimentString = sentimentString.substring(0,space);
+        console.log("Sentiment: " + sentimentString +" Magnitude:" + magnitude);
+        stock.sentimentAvg = sentimentString;
+        stock.magnitude = magnitude;
+      });
+    
+
     }
   }
 }
