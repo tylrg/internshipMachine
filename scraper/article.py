52da8c817b9d90e9c09dae1fb86cb000b381af94
Jesse White <jwhite48@binghamton.edu>
2020-02-08 22:12:35 -0500

    Added two scrapers and helper class
    Seekingalpha and marketwatch scrapers are added. Article helper class makes the construction of relevant article objects possible. Minor tweaks needed to implement with user input.

---
+++ scraper/article.py
@@ -0,0 +1,17 @@
+class Article:
+    sentimentScore = 0
+    magnitude = 0
+    
+    def __init__(self, title, url, body):
+        self.title = title
+        self.url = url
+        self.body = body
+
+    def setSentimentScore(self, ss):
+        self.sentimentScore = ss
+
+    def setMagnitude(self, m):
+        self.magnitude = m
+
+    def __str__(self):
+        return self.title + '\n' + self.url + '\n' + self.body
