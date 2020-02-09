dd41872a96885e67959a5fbebe6894a3f3698c1d
bwilso14 <bwilso14@binghamton.edu>
2020-02-08 22:13:37 -0500

    basicweb

--- server/main.py
+++ server/main.py
@@ -1,17 +1,19 @@
 from flask import Flask
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api,reqparse,request
 
 import requests
 import yfinance as yf
 import pandas as pd
 import numpy as np
 
+parser=reqparse.RequestParser()
+parser.add_argument('symbol')
 
 app = Flask(__name__)
 api = Api(app)
 
 
-def returnPrice(symbol):
+def returnPercent(symbol):
     stock = yf.Ticker(symbol)
     h = stock.history(period="5d")
     x = pd.DataFrame(h)
@@ -24,12 +26,44 @@ def returnPrice(symbol):
     print(value)
     return value
 
-class HelloWorld(Resource):
-    def get(self):
-        value = returnPrice("XOM")
-        return {'hello': value }
+def returnPrice(symbol):
+    stock = yf.Ticker(symbol)
+    h = stock.history(period="5d")
+    x = pd.DataFrame(h)
+    new = x.iloc[0]
+    #old = x.iloc[4]
+    y = new.iloc[3]
+    #z = old.iloc[3]
+    #value = ((y-z)/y)*100
+    value = round(y, 2)
+    #print(value)
+    return value
 
-api.add_resource(HelloWorld, '/')
+class Percent(Resource):
+    def get(self):
+        # value = returnPrice("XOM")
+        # return {'hello': value }
+        symbol=request.args.get('symbol')
+        value = returnPercent(symbol)
+        return {
+            "symbol": symbol,
+            "value":value
+        }
+
+class Price(Resource):
+    def get(self):
+        # value = returnPrice("XOM")
+        # return {'hello': value }
+        symbol=request.args.get('symbol')
+        value = returnPrice(symbol)
+        return {
+            "symbol": symbol,
+            "value":value
+        }
+
+api.add_resource(Percent, '/percent')
+api.add_resource(Price, '/price')
+
 
 if __name__ == '__main__':
     app.run(debug=True)
